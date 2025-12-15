1fdccd8126656c2619253cef46c6e09d7ddd8b0e
Amr <240349211+reconfirmok@users.noreply.github.com>
2025-12-15 03:17:45 +0200

    refactor: main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
@@ -21,8 +20,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-        time_passed = clock.tick(60)
-        dt = time_passed / 1000
+        # limit FPS to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
